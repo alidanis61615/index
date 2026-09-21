935cfdb6fecb2605770bc0c3727204282232e2c6
thecodewithali <alidanis61615@gmail.com>
2026-09-21 20:34:53 +0300

    feat: add persistent dark mode

---
+++ projects/taskflow/lib/controllers/theme_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeController extends ChangeNotifier {
+  static const _themeKey = 'taskflow_dark_mode';
+
+  bool _isDarkMode = false;
+  bool get isDarkMode => _isDarkMode;
+
+  Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_themeKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggle() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, _isDarkMode);
+  }
+}
